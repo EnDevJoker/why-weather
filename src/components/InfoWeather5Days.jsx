82296c1f0273331c96fb7d2ba6e0b403e3a50cea
Diego Brito <diegobritto50@hotmail.com>
2024-11-26 01:19:44 -0300

    App Weather + React

--- src/components/InfoWeather5Days.jsx
+++ src/components/InfoWeather5Days.jsx
@@ -13,18 +13,27 @@ function InfoWeather5Days({ weather5Days }) {
     }
     const next5Days = Object.values(dailyInfo).slice(1,6)
 
+    function convertDate(date) {
+
+        const newDate = new Date(date.dt * 1000).toLocaleDateString('pt-BR', {weekday: 'long', month: 'numeric', day: 'numeric', year: 'numeric'})
+        return newDate;
+    }
+
+
     return (
         <div className='weather-container'>
-            <p>5 Days</p>
+            <h3>Previsão dos próximos 5 Dias</h3>
+            <div className='weather-list'>
             {next5Days.map(forecast => (
-                <div key={forecast.dt}>
+                <div key={forecast.dt} className='weather-item'>
                      
-                    <p>Domingo</p>    
+                    <p className='forecast-day'>{convertDate(forecast)}</p>
                     <img src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`} alt="" /> 
-                     <p>{forecast.weather[0].description}</p>
+                     <p className='forecast-description'>{forecast.weather[0].description}</p>
                      <p>{Math.round(forecast.main.temp_min)}°C Min / {Math.round(forecast.main.temp_max)}°C Máx</p>
                 </div>
             ))}
+            </div>
     </div>
     )
 
